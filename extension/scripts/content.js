e6560d6c6fb5e9a20ce533e9a30ef2ac643a9e3d
Louise <louisegleibbrandt@gmail.com>
2023-03-21 14:37:19 +0100

    Added simple display functionality of food group (right now just second element of breadcrumb-trail

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -8,12 +8,42 @@ if (ingredientsDiv) {
   }
 }
 
+// Create div item to display label
+
 const breadcrumbTrail = document.getElementsByClassName("breadcrumb-trail").item(0);
 
 if (breadcrumbTrail) {
+  a = document.createElement("DIV");
   const breadcrumbTrailItemList = breadcrumbTrail.childNodes;
   console.log("FOOD GROUPS:\n");
   for (i = 0; i < breadcrumbTrailItemList.length; i++) {
-    console.log(breadcrumbTrailItemList.item(i).childNodes.item(0).childNodes.item(0));
+    console.log(breadcrumbTrailItemList.item(i).childNodes.item(0).textContent);
   }
+  const itemCategory = breadcrumbTrailItemList.item(1).childNodes.item(0).textContent.replace(/\s/g, '');
+  a.appendChild(document.createTextNode("Food Group: \n" + itemCategory+"\n\n"));
+
+  // Simple switch statement to check for food groups
+  a.appendChild(document.createTextNode("Total CO2:")) 
+  switch(itemCategory) {
+    case 'Chocolade':
+      a.appendChild(document.createTextNode("3.97"))
+      break;
+    default:
+      console.log(itemCategory)
+      break;
+  }
+
+  a.style.width = "15%";
+  a.style.height = "10%";
+  a.style.position = "absolute";
+  a.style.top = "10px";
+  a.style.left = "10px";
+  a.style.backgroundColor = "lightblue";
+  a.style.zIndex = 9999999;
+  a.style.font = "italic bold 16px arial,serif";
+
+  document.body.appendChild(a);
+  document.body.style = "white-space: pre;"
 }
+
+
